c4c77aed68616e75c1b39d518e5e88199b55f221
Yonattan <yonattan_hope@yahoo.com>
2022-12-22 09:01:34 +0700

    layar ud selesai

--- lib/feature/login/presentation/login_screen.dart
+++ lib/feature/login/presentation/login_screen.dart
@@ -12,103 +12,111 @@ class LoginScreen extends StatefulWidget {
     String userId ="";
     String password = "";
       
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.asset(
-                "assets/images/login/header-login.png",
-                // fit: BoxFit.fill,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  gapH32,
-                  Image.asset(
-                    "assets/images/logo.png",
-                    // fit: BoxFit.fill,
-                  ),
-                ],
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(64.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+    return SafeArea(
+      child: Scaffold(
+        body: ListView(
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("Login",
-                    style: Theme.of(context).textTheme.headline4!.copyWith(
-                          fontWeight: FontWeight.w600,
-                          color: Colors.black,
-                        )),
-                gapH8,
-                Text(
-                  "Please sign in to continue.",
-                  style: Theme.of(context).textTheme.bodyLarge,
+                Image.asset(
+                  "assets/images/login/header-login.png",
+                  // fit: BoxFit.fill,
                 ),
-                gapH24,
-                LieTextField(
-                  label: "User Id",
-                  // labelStyle: Theme.of(context).textTheme.bodyText2!.copyWith(
-                  //       color: Colors.black,
-                  //       // fontSize: 12,
-                  //     ),
-                  onChanged: ((value) {
-                    userId = value;
-                  }),
-                ),
-                gapH8,
-                LieTextField(
-                  label: "Password",
-                  obscure: true,
-                  maxLines: 1,
-                  // labelStyle: Theme.of(context).textTheme.bodyText2!.copyWith(
-                  //       color: Colors.black,
-                  //       // fontSize: 12,
-                  //     ),
-                  onChanged: ((value) {
-                    password = value;
-                  }),
-                ),
-                gapH8,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.purple,
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(16)),
-                        ),
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 24),
-                        child: Text('Login'),
-                      ),
-                      onPressed: () => controller.doLogin(userId, password),
+                    gapH32,
+                    Image.asset(
+                      "assets/images/logo.png",
+                      // fit: BoxFit.fill,
                     ),
                   ],
                 ),
               ],
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Don't have an account? "),
-              TextButton(
-                onPressed: () => controller.doLogin(userId, password),
-                child: Text("Sign Up"),
+            Padding(
+              padding: const EdgeInsets.all(64.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Login",
+                      style: Theme.of(context).textTheme.headline4!.copyWith(
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black,
+                          )),
+                  gapH8,
+                  Text(
+                    "Please sign in to continue.",
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  gapH24,
+                  LieTextField(
+                    label: "User Id",
+                    // labelStyle: Theme.of(context).textTheme.bodyText2!.copyWith(
+                    //       color: Colors.black,
+                    //       // fontSize: 12,
+                    //     ),
+                    onChanged: ((value) {
+                      userId = value;
+                    }),
+                  ),
+                  gapH8,
+                  LieTextField(
+                    label: "Password",
+                    obscure: true,
+                    maxLines: 1,
+                    // labelStyle: Theme.of(context).textTheme.bodyText2!.copyWith(
+                    //       color: Colors.black,
+                    //       // fontSize: 12,
+                    //     ),
+                    onChanged: ((value) {
+                      password = value;
+                    }),
+                  ),
+                  gapH8,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.purple,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(16)),
+                          ),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 24),
+                          child: Text('Login'),
+                        ),
+                        onPressed: () => controller.doLogin(userId, password),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            ],
-          )
-        ],
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height /2,
+              child: Expanded(
+                flex: 1,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Don't have an account? "),
+                    TextButton(
+                      onPressed: () => controller.doLogin(userId, password),
+                      child: const Text("Sign Up"),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
